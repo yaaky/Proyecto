3786be468730f4ff49b7c67d0b706412b3f26edc
yaaky <jacquelinemoraaguilar6@gmail.com>
2025-02-14 12:54:43 -0600

    avancedia3

--- tictac.js
+++ tictac.js
@@ -1,39 +1,99 @@
 
 
 const cells = document.getElementsByClassName("cell");
-const turnoDisplay = document.getElementById("turno");
-const mensaje = document.getElementById("mensaje");
+const mensajeGane = document.getElementById("mensaje");
+// const mensaje = document.getElementById("mensaje");
+
 const botonReiniciar = document.getElementById("reiniciar");
 
+const jugadoX = document.getElementById("jugadorX");
+const jugadorO = document.getElementById("jugadorO");
+
+
+let turno = "jugador 1";
+let juego = true;
+
+let ganeX = 0;
+let ganeO = 0;
+
+let btns = []
 
 for (let i = 0; i < cells.length; i++){
+    let celda = cells[i];
 
-cells[i].addEventListener("click",function name() {
-    cells[i].innerHTML="X"
+    btns.push(celda)
 
-    //realizar fonciunalidad que hace que la computadora juege usando el math randon.
-    let random = Math.floor(Math.random() * 9)
-    cells[random].innerHTML="O"
-    cells[i].removeEventListener("click", name)
-  
-
-
-
-    turnoDisplay.innerHTML="Turno Jugador 2"
-
-
-    mensaje.innerHTML="El jugador 1 ha ganado"
-
-
-    
-    
-    botonReiniciar.addEventListener("click", function(){
-        location.reload("click")
-
-
+    celda.addEventListener("click", function () {
         
+        if(juego == true) {
+            
+                    if(turno == "jugador 1") {
+                        if(celda.textContent.trim() === "") {
+            
+                            turno = "jugador 2"
+                
+                            celda.textContent = "x"
+                            ganador()
+                        }
+                    }
+                    else {
+                        if(celda.textContent.trim() === "") {
+            
+                            turno = "jugador 1"
+                
+                            celda.textContent = "o"
+                            ganador()
+                        }
+            
+                    }
+
+        }
+
+      
     })
-    
-})
 
 }
+
+function ganador(){
+    const combinacionesGanadoras = 
+    [ [0,1,2], [3,4,5], [6,7,8], //Filas
+    [0,3,6], [1,4,7], [2,5,8], // Columnas
+    [0,4,8], [2,4,6]] //Diagonales
+
+    for (let combinacion of combinacionesGanadoras) {
+        if (btns[combinacion[0]].textContent === "x" && btns[combinacion[1]].textContent === "x" && btns[combinacion[2]].textContent === "x") {
+            mensajeGane.textContent = "Ha ganado - X";
+            ganeX++;
+
+            jugadoX.textContent = ganeX;
+
+
+            juego = false
+       
+        }
+
+        if (btns[combinacion[0]].textContent === "o" && btns[combinacion[1]].textContent === "o" && btns[combinacion[2]].textContent === "o") {
+             mensajeGane.textContent = "Ha ganado - O";
+             juego = false
+
+             
+        }
+    
+        
+        else {
+
+            console.log(btns);
+            
+            if(btns.length == 0 && juego == true) {
+                mensajeGane.textContent = "Empate";
+            }
+        
+        }
+    }
+
+}
+
+
+botonReiniciar.addEventListener("click", function() {
+    location.reload()
+})
